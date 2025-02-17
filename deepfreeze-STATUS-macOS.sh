b6b101dabb682d28d5adab37890d8c2625393ed3
Montel Gray <mgray@choate.edu>
2025-02-17 11:01:29 -0500

    1.2.0

--- deepfreeze-STATUS-macOS.sh
+++ deepfreeze-STATUS-macOS.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
 # Created by MVG (@PBandJamf) for Choate Rosemary Hall
+# Queries Deep Freeze status and returns either Frozen, Thawed, or "Not Installed"
 
 # CHANGELOG
+
+# Version 1.2.0
+# - Further refined status interpretation
+
 # Version 1.1.0
 # - Using the /usr/local binary now instead.
 # - Fine-tuned the status code interpretation logic.
 
 # Version 1.0.0
 # - Initial release
-# Queries Deep Freeze status and returns either Frozen, Thawed, or Deep Freeze Not Installed
 
 # Get Deep Freeze status
 STATUS_CODE=$(/usr/local/bin/deepfreeze status --thawed 2>/dev/null)
 
-# Interpret status code
-case "$STATUS_CODE" in
-    0)
+# Interpret status output
+case "$STATUS_OUTPUT" in
+    "Thawed")
         STATUS="Thawed"
         ;;
-    1)
+    "Thawed but restart required")
         STATUS="Thawed but restart required"
         ;;
-    2)
+    "Frozen")
         STATUS="Frozen"
         ;;
     *)
-        STATUS="Error: Unknown status code ($STATUS_CODE)"
+        STATUS="Error: Unknown status ($STATUS_OUTPUT)"
         ;;
 esac
 
